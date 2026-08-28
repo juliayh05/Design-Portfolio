d9716610fb78446c80a969d05a88fe2663ec7e17
Julia Heo <juliayheo@gmail.com>
2026-08-28 12:19:55 -0700

    Update homepage, work page, footer, and animations

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+		extend: {
+			fontFamily: {
+				serif: ['"Instrument Serif"', 'serif'],
+				sans: ['"Work Sans"', 'system-ui', 'sans-serif'],
+			},
+		},
+	},
+};
